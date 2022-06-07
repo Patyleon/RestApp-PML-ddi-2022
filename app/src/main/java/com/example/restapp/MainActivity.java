4aa36f1050762ae10cbd5237a51e11563127a938
Patricia Mendez Leon <phefet@gmail.com>
2022-06-07 11:59:53 -0500

    agregada lista

--- app/src/main/java/com/example/restapp/MainActivity.java
+++ app/src/main/java/com/example/restapp/MainActivity.java
@@ -6,18 +6,24 @@ import android.widget.TextView;
 
 import com.example.restapp.databinding.ActivityMainBinding;
 
+import java.util.Arrays;
+
 public class MainActivity extends Activity {
 
     private TextView mTextView;
-    private ActivityMainBinding binding;
+    private Butoon mButoon;
+    private final List<String> mensajes = Arrays.asList(
+            "Edad",
+            "sexo",
+            "nacimiento",
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+       mTextView = findViewById(R.id.mensajeTextview);
+        mButoon = findViewById(R.id.aceptarButton);
 
-        mTextView = binding.text;
     }
 }
